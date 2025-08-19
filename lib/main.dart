55ab90f317ab4d2305c8291bfa1a4ea32b7600b4
hassanrazzaq8 <iamhassanrazzaq@gmail.com>
2025-08-19 23:59:17 +0100

    bugs fixes

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'common/utils.dart';
-// import 'l10n/app_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
