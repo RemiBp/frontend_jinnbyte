35baa9a390087f0ce078c55a618e314b1601543c
Arhamss <syed.arhamss1@gmail.com>
2025-09-29 15:08:00 +0500

    - pull from auth apis branch. - pull from feed apis branch.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'common/utils.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'l10n/app_localizations.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
